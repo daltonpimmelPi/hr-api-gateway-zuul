52055d28d753b3f88498a86f6337c684c4bd5542
Dalton <dalton@viasoft.com.br>
2021-01-18 22:30:56 -0300

    routes api gatewar microservices

--- src/main/java/com/system/hrapigatewayzull/HrApiGatewayZullApplication.java
+++ src/main/java/com/system/hrapigatewayzull/HrApiGatewayZullApplication.java
@@ -2,8 +2,12 @@ package com.system.hrapigatewayzull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient //tambem é um client eureka
 public class HrApiGatewayZullApplication {
 
 	public static void main(String[] args) {
