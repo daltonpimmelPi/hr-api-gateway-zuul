3ca283f64007a4dc32fcc889bf3cb0752a481039
Dalton <dalton@viasoft.com.br>
2021-01-21 00:17:21 -0300

    configured jwt token, resource server

---
+++ src/main/java/com/system/hrapigatewayzull/config/ResourceServerConfig.java
@@ -0,0 +1,45 @@
+package com.system.hrapigatewayzull.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    //ROTAS PUBLICAS, APENAS A ROTA DE LOGIN
+    private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
+
+    //ROTAS AUTORIZADAS APENAS PARA OPERADOR
+    private static final String[] OPERATOR = { "/hr-worker/**" };
+
+    private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**" };
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //cara que vai configurar as autorizações
+
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated(); //qualquer outra rota vai ter que estar autenticado
+
+    }
+
+
+}
